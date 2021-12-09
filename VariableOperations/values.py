19ad8092a83de838c0175cedd119e365caf7614c
Carrington <cwm4ec@virginia.edu>
2021-12-09 15:16:34 -0500

    update

--- VariableOperations/values.py
+++ VariableOperations/values.py
@@ -1,11 +1,11 @@
 ## Section 1
-number = input("Enter an integet value: ")
+number = input("Enter an integer value: ")
 # this line of code multiplies by 2
 number *= 2
 print(number)
 
 ## Section 2
 import cs50
-value = cs50.get_int("Enter an integer value:")
+value = cs50.get_int("Enter an integer value: ")
 value *= 2
 print(value)
